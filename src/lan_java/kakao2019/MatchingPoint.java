28bcba418fafd9d56cafa88f5a1d009812830707
hyunro19 <hyunro19@gmail.com>
2020-09-11 22:03:07 +0900

    200911 | added prbms in kakao 2019

---
+++ src/lan_java/kakao2019/MatchingPoint.java
@@ -0,0 +1,125 @@
+package lan_java.kakao2019;
+
+import java.util.*;
+
+public class MatchingPoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "aabcde";
+
+		System.out.println(s.indexOf("b"));
+	}
+	
+    public int solution(String word, String[] pages) {
+        Map<String, Page> map = new HashMap<>(); // 사이트 -> [기본, 외부링크수, 피링크수, 매칭점수]
+        
+        for(String page : pages) {
+            // 1. 현재사이트 주소 얻기
+        	int a = page.indexOf("<meta property=\"og:url\" content=\"")+"<meta property=\"og:url\" content=\"".length();
+            int b = page.indexOf("\"/>\n</head>");
+            String thisUrl = page.substring(a, b);
+            if(map.get(thisUrl)== null) map.put(thisUrl, new Page());
+            
+            // 2. 단어개수(기본점수) 
+            // 2-1. body와 /body 사이 추출
+            int c = page.indexOf("<body>")+"<body>".length();
+            int d = page.indexOf("</body>");
+            String body = page.substring(c, d);
+            
+            int h = 0;
+            int e = body.indexOf("<a href=\"")+"<a href=\"".length();
+            StringBuilder text = new StringBuilder();
+            int numOfLinkTo = 0;
+            
+            while(e!=-1) {
+            	text.append(body.substring(h, e)); // 링크 이전부분까지 text만 추출
+            	
+            	int f = body.indexOf("\"></a>", e);
+            	String linkTo = body.substring(e, f);            	
+                numOfLinkTo++;                                               
+                h = f+"\"></a>".length();
+                e = body.indexOf("<a href=\"", h)+"<a href=\"".length();                                                   
+                
+                if(map.get(linkTo)==null) map.put(linkTo, new Page());
+                Page p = map.get(linkTo);
+                p.inward.add(thisUrl);
+            }
+            text.append(body.substring(h)); // 마지막 링크 뒤쪽 텍스트도 추가                                
+             // 이제 온전한 텍스트를 얻었다.
+            
+            // 모두 소문자로 변환
+            String lowerText = text.toString().toLowerCase();
+            String lowerWord = word.toLowerCase();
+            int freq = 0;
+            int g = lowerText.indexOf(lowerWord);            
+             while(g!=-1) {
+            	 
+                 if(g!=0) {
+                	 char m = lowerText.charAt(g-1);
+                	 if(m<97 || m>122) {
+                		 g = lowerText.indexOf(lowerWord, g+lowerText.length());
+                		 continue;
+                	 }                	 
+                 }
+                 if(g+lowerWord.length()<lowerText.length()) {
+                     char n = lowerText.charAt(g+lowerWord.length()); // word 바로 다음 character                	 
+                	 if(n<97 || n>122) {
+                		 g = lowerText.indexOf(lowerWord, g+lowerText.length());
+                		 continue;
+                	 }     
+                 }
+                 ++freq;
+                 g = lowerText.indexOf(lowerWord, g+lowerText.length());
+                     //둘다 아니면 ++기본점수 추가
+             } 
+             Page p = map.get(thisUrl);
+             p.basic = freq;
+             p.outward = numOfLinkTo;
+             // 이렇게 url, 기본점수, 외부링크수를 얻었다.
+        }
+
+        for(Page v : map.values()) {
+        	ArrayList<String> inward = v.inward;
+        	for(String s : inward) {
+        		Page vv = map.get(s);
+        		v.match=(double)v.basic/(double)v.outward;
+        	}
+        }
+        // 4. 링크점수 & 매칭점수 구하기
+        // 마지막 map 순회를 다시 한 번 해야될 듯
+        
+        int x = 0;
+        Page[] pp = new Page[map.values().size()];
+        for(Page v : map.values()) {
+            pp[x++]=v;
+        }
+        Arrays.sort(pp, new Comparator<Page>() {
+
+			@Override
+			public int compare(Page o1, Page o2) {
+				if(o1.match==o2.match) {
+					return o1.idx-o2.idx;
+				} else {
+					return (int)(o2.match-o1.match);
+				}				
+			}
+        	
+        });
+        
+        int answer = 0;
+        return answer;
+    }
+}
+
+class Page {
+	int idx = 999;
+	int basic;
+	int outward;
+	ArrayList<String> inward;
+	double match;
+	Page() {
+		this.inward = new ArrayList<>();
+	}
+	
+}
