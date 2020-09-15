58bb78848d9c4c7504f6e121af0f5d1e8a47ca93
hyunro19 <hyunro19@gmail.com>
2020-09-15 17:00:29 +0900

    200915 | added ShuttleBus in kakao2018

---
+++ src/lan_java/kakao2018/ShuttleBus.java
@@ -0,0 +1,15 @@
+package lan_java.kakao2018;
+
+public class ShuttleBus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    public String solution(int n, int t, int m, String[] timetable) {
+        String answer = "";
+        return answer;
+    }
+
+}
