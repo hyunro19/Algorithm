dcb17f891f12d82267cb323bbd1b1ce57bd9c2b1
hyunro19 <hyunro19@gmail.com>
2020-09-25 20:53:50 +0900

    200925 | added RoomAssignment in kakao_intern_2019_FW

---
+++ src/lan_java/kakao_intern_2019_FW/RoomAssignment.java
@@ -0,0 +1,15 @@
+package lan_java.kakao_intern_2019_FW;
+
+public class RoomAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    public long[] solution(long k, long[] room_number) {
+        long[] answer = {};
+        return answer;
+    }
+
+}
