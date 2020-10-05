db8b2a2d1d228fa88adae62a916aa7194dc2045d
hyunro19 <hyunro19@gmail.com>
2020-10-05 09:10:34 +0900

    201005 | added N2606_Virus in bj

---
+++ src/jv/bj/lv3/N2606_Virus.java
@@ -0,0 +1,18 @@
+package jv.bj.lv3;
+
+// https://www.acmicpc.net/problem/2606
+import java.util.*;
+
+public class N2606_Virus {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int nextInt = sc.nextInt();
+		while(sc.hasNext()) {
+			String nextLine = sc.nextLine();
+		}
+		
+		sc.close();
+	}
+
+}
